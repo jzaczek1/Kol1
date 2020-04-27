dc59925a2cf23fb1017b09517495f5209ee71f02
jzaczek1 <s18593@pjwstk.edu.pl>
2020-04-27 21:14:36 +0200

    Przygotowanie

---
+++ kol1/kol1/Controllers/MyController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kol1.Controllers
+{
+    [Route("api/kolokwium")]
+    [ApiController]
+    public class MyController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult get()
+        {
+            return Ok("Działa");
+        }
+        [HttpGet("{id}")]
+        public IActionResult getSth(int id)
+        {
+            return Ok(id);
+        }
+    }
+}
